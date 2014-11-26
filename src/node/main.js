e709b61fb08bffdf3e52d100421f983706c2bf3a
Bill Boyer <william.boyer.nyc@gmail.com>
2014-11-26 15:43:01 -0500

    First integration with airports-db.

--- src/node/main.js
+++ src/node/main.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
+require('airports-db').addRoutes(app);
 
 require('facts-db').addRoutes(app);
 
